3e1390a7db46a2efd37e4e97fcd97396736d5b77
eishynena <deisyjaque1992@gmail.com>
2014-11-08 17:55:43 -0430

    Ultimo cambio
    Se agregó perfil de usuario con menú

--- Proyecto/Fase_2/js/funciones.js
+++ Proyecto/Fase_2/js/funciones.js
@@ -39,3 +39,19 @@ function volver(){
 	// redirigir  la pagina menu.html
 	document.location.href="../index.html";
 }
+
+$("intento1.html").ready(function() {
+$(".contenido_tab").hide(); //Ocultar capas
+$("ul.tabs li:first").addClass("activa").show(); //Activar primera pestaña
+$(".contenido_tab:first").show(); //Mostrar contenido primera pestaña
+
+// Sucesos al hacer click en una pestaña
+$("ul.tabs li").click(function() {
+$("ul.tabs li").removeClass("activa"); //Borrar todas las clases "activa"
+$(this).addClass("activa"); //Añadir clase "activa" a la pestaña seleccionada
+$(".contenido_tab").hide(); //Ocultar todo el contenido de la pestaña
+var activatab = $(this).find("a").attr("href"); //Leer el valor de href para identificar la pestaña activa 
+$(activatab).fadeIn(); //Visibilidad con efecto fade del contenido activo
+return false;
+});
+});
